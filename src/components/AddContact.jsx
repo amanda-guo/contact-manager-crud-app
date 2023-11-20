febf8bd68f83f9b722beabe041ce2ebae27f5c60
amanda-guo <aguo55@hotmail.com>
2023-11-20 14:41:36 -0500

    more styling

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,49 +1,166 @@
-import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import PrimaryButton from "./PrimaryButton";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import "./AddContact.css";
+import FormExample from "./FormExample";
 
 function AddContact() {
+  const [successMsg, setSuccessMsg] = useState("");
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
+    reset,
   } = useForm();
 
   const onSubmit = (data) => {
-    axios.post("http://localhost:5000/contacts/", data).then((res) => {
-      console.log(res);
-      console.log(res.data);
-    });
+    axios
+      .post("http://localhost:5000/contacts/", data)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setSuccessMsg("Contact added successfully.");
+        reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control">
-          <label>First Name: </label>
-          <input type="text" name="firstName" {...register("firstName")} />
-        </div>
-        <div className="form-control">
-          <label>Last Name: </label>
-          <input type="text" name="lastName" {...register("lastName")} />
-        </div>
-        <div className="form-control">
-          <label>Phone Number: </label>
-          <input type="text" name="phoneNumber" {...register("phoneNumber")} />
-        </div>
-        <div className="form-control">
-          <label>Email: </label>
-          <input type="text" name="email" {...register("email")} />
-        </div>
-        <div className="form-control">
-          <label>Address: </label>
-          <input type="text" name="address" {...register("address")} />
-        </div>
-        <div className="form-control">
-          <button type="submit">Add</button>
-        </div>
-      </form>
-      <PrimaryButton type="outline-primary" text="Back" link="/" />
+      <FormExample />
+      <Container>
+        <Form>
+          <h1>Add a Contact</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {successMsg && <p className="success-msg">{successMsg}</p>}
+            <Controller
+              name="firstName"
+              control={control}
+              render={({ field }) => (
+                // <Form.Group
+                //   {...field}
+                //   className="mb-3"
+                //   controlId="formBasicText"
+                // >
+                //   <Form.Label>First Name</Form.Label>
+                //   <Form.Control
+                //     required
+                //     type="text"
+                //     placeholder="Enter first name"
+                //   />
+                // </Form.Group>
+                <Form.Group
+                  {...field}
+                  as={Col}
+                  md="4"
+                  controlId="validationCustomFirstName"
+                >
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Enter first name"
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please enter a first name.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              )}
+            />
+            {/* <div className="form-control">
+              <label>First Name</label>
+              <br></br>
+              <input
+                type="text"
+                name="firstName"
+                {...register("firstName", {
+                  required: "First Name is required.",
+                })}
+              />
+              {errors.firstName && (
+                <p className="errorMsg">{errors.firstName.message}</p>
+              )}
+            </div> */}
+            <div className="form-control">
+              <label>Last Name</label>
+              <br></br>
+              <input
+                type="text"
+                name="lastName"
+                {...register("lastName", {
+                  required: "Last Name is required.",
+                })}
+              />
+              {errors.lastName && (
+                <p className="errorMsg">{errors.lastName.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <label>Phone Number</label>
+              <br></br>
+              <input
+                type="tel"
+                name="phoneNumber"
+                {...register("phoneNumber", {
+                  required: "Phone Number is required.",
+                  pattern: {
+                    value: /^[0-9]{10}$/,
+                    message:
+                      "Phone number is not valid. Please enter 10 digits with no other characters.",
+                  },
+                })}
+              />
+              {errors.phoneNumber && (
+                <p className="errorMsg">{errors.phoneNumber.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <label>Email</label>
+              <br></br>
+              <input
+                type="email"
+                name="email"
+                {...register("email", {
+                  required: "Email is required.",
+                  pattern: {
+                    value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                    message: "Email is not valid.",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="errorMsg">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <label>Address</label>
+              <br></br>
+              <input
+                type="text"
+                name="address"
+                {...register("address", {
+                  required: "Address is required.",
+                })}
+              />
+              {errors.address && (
+                <p className="errorMsg">{errors.address.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <button type="submit">Add</button>
+            </div>
+          </form>
+          <PrimaryButton type="outline-primary" text="Back" link="/" />
+        </Form>
+      </Container>
     </>
   );
 }
---
+++ src/components/FormExample.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+function FormExample() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Container>
+      <h1>Add a Contact</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustomFirstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control required type="text" placeholder="Enter first name" />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please enter a first name.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustomLastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control required type="text" placeholder="Enter last name" />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please enter a last name.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustomPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter phone number"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid phone number.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustomEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control required type="text" placeholder="Enter email" />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustomAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control required type="text" placeholder="Enter address" />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please enter an address.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Button type="submit">Submit form</Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default FormExample;
